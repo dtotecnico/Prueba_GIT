d9662cc2d86f9301ebf744fde80eb1a9a15dfbb0
dpatik <dpatik@PC-Daniel-W7.toledo.com>
2016-08-22 11:26:01 -0300

    primer cambio

---
+++ src/prueba_ver/PRUEBA_VER.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_ver;
+
+/**
+ *
+ * @author dpatik
+ */
+public class PRUEBA_VER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esto es una prueba de Versionado");
+        System.out.println("Esta es otra prueba");
+    }
+}
